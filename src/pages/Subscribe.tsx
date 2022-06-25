90733f40d970d24aa2b7d1a1feee7e9791dbb285
Vilson Sampaio <vilson.sampaio04@gmail.com>
2022-06-25 19:59:49 -0300

    aula 05: adicionando graphql code generator

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -3,15 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { gql, useMutation } from "@apollo/client";
 
 import { Logo } from "../components/Logo";
-
-
-const CREATE_SUBSCRIBER_MUTATION = gql`
-  mutation CreateSubscriber  ($name: String!, $email: String!) {
-    createSubscriber(data: {name: $name, email: $email}) {
-      id
-    }
-  }
-`;
+import { useCreateSubscriberMutation } from "../graphql/generated";
 
 export function Subscribe() {
   const navigate = useNavigate();
@@ -19,7 +11,7 @@ export function Subscribe() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
 
-  const [createSubscriber, { loading }] = useMutation(CREATE_SUBSCRIBER_MUTATION);
+  const [createSubscriber, { loading }] = useCreateSubscriberMutation();
 
   async function handleSubscribe(event: FormEvent) {
     event.preventDefault();
